912d64ca30468fe354abc9bd1d95c700083e4dbd
samer-- <samer.abdallah.00@gmail.com>
2019-01-16 19:19:51 +0000

    Add flip/4

--- prolog/swimpd/tools.pl
+++ prolog/swimpd/tools.pl
@@ -1,5 +1,5 @@
 :- module(tools, [(+)//1, (*)//1, parse_head//2, nat//1, num//1, atom//1, pphrase/1, quoted//1, quoted//2,
-                  decimal//0, report//1, report//2, select_nth/4, maybe//2, maybe/2, fmaybe/3, fjust/3]).
+                  decimal//0, report//1, report//2, select_nth/4, maybe//2, maybe/2, fmaybe/3, fjust/3, flip/4]).
 
 :- use_module(library(dcg_core), [(//)//2, list//1]).
 :- use_module(library(dcg_codes), [esc//2, ctype//1, fmt//2]).
@@ -14,6 +14,7 @@ report(Name-Value) --> report(Name, Value).
 report(Name, Value) --> fmt('~w: ~w\n', [Name, Value]).
 
 select_nth(N, X, L1, L2) :- nth0(N, L1, X, L2).
+flip(P, X, Y, Z) :- call(P, Y, X, Z).
 
 :- meta_predicate +(//,?,?), +(//,?,?), pphrase(//), quoted(3,?,?,?), quoted(//,?,?).
 pphrase(P) :- phrase(P, C), format('~s', [C]).
