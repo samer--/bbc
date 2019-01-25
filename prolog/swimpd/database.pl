1099794c574ceab2b8f1bbcf000e393b75db9b4b
samer-- <samer.abdallah.00@gmail.com>
2019-01-25 11:50:45 +0000

    Add in progress directory

--- prolog/swimpd/database.pl
+++ prolog/swimpd/database.pl
@@ -6,10 +6,8 @@
       Artist = ? channel? BBC?
       memoise, DB version?
       Shelf for keeping programmes
-      In progress for programmes part listened to
       Allow adding arbitrary PID
       Try putting PID/ID in multiple places in file tree
-      memoise db stats: number of programmes, number of brands
       confirm URL is ok and try another if not.
  */
 
@@ -17,6 +15,7 @@
 :- use_module(library(memo)).
 :- use_module(library(dcg_core),  [maybe/3]).
 :- use_module(asyncu, [spawn/1]).
+:- use_module(state,  [state/2]).
 :- use_module(tools,  [sort_by/3, report//1, report//2, maybe/2, maybe//2]).
 :- use_module(bbc(bbc_tools), [log_and_succeed/1]).
 :- use_module(bbc(bbc_db), [service/3, time_service_schedule/3, service_schedule/2, service_live_url/2, service_entry/2,
@@ -28,36 +27,35 @@ id_pid(Id,PID) :- once(browse(pid_id(PID, Id))).
 is_programme(PID) :- \+service(PID, _, _).
 
 % --- adding to playlist by path  ---
-addid([LongName], nothing) -->
-   {longname_service(LongName, S), findall(E, service_entry(S, E), Entries)},
-   foldl(add(LongName), Entries).
-
-addid([LongName, PID], just(Id)) -->
-   {longname_service(LongName, S), service_entry(S, E), prop(E, pid(PID)), pid_id(PID, Id)},
-   add(LongName, E).
-
+addid([Directory], nothing) --> {directory(Directory, Entries)}, foldl(add(Directory), Entries).
 addid(['Live Radio'], nothing) --> {live_services(Services)}, foldl(add_live, Services).
+
 addid(['Live Radio', LongName], just(Id)) --> {live_service(S, LongName), pid_id(S, Id)}, add_live(S-LongName).
+addid(['In Progress', PID], just(Id)) --> {once(service_entry_pid(_, E, PID)), pid_id(PID, Id)}, add('In Progress', E).
+addid([LongName, PID], just(Id)) -->
+   {longname_service(LongName, S), service_entry_pid(S, E, PID), pid_id(PID, Id)},
+   add(LongName, E).
 
 add_live(S-SLN) --> {live_service_tags(S-SLN, Tags), live_url(S, URL)}, [song(S, URL, Tags)].
 add(LongName, E) --> {entry_tags(LongName, E, PID, Tags, []), entry_xurl(redir(dash), E, _-URL)}, [song(PID, URL, Tags)].
 
 % --- query db contents ---
 lsinfo([]) -->
-   report(directory, 'Live Radio'),
+   foldl(report(directory), ['In Progress', 'Live Radio']),
    {all_services(Services)}, foldl(service_dir, Services).
 lsinfo(['Live Radio']) --> {live_services(Services)}, foldl(live_radio, Services).
-lsinfo([ServiceName]) -->
-	{ longname_service(ServiceName, S),
-     findall(Item, service_sortkey_entry(S, Item), Items),
-     keysort(Items, SortedItems)
-	},
-	foldl(programme(ServiceName), SortedItems).
+lsinfo([Directory]) --> {directory(Directory, Items)}, foldl(programme(Directory), Items).
 
-service_sortkey_entry(S, k(SortedParents, Date)-E1) :-
-   bagof(E, (service_entry(S, E), prop(E, pid(_))), [E1|_]),
-   prop(E1, broadcast(Date)),
-   findall(T-N, prop(E1, parent(_, T, N)), Parents),
+directory('In Progress', Items) :-
+   findall(E, (state(position(PID), _), once(service_entry_pid(_, E, PID))), Items).
+directory(ServiceName, SortedItems) :-
+	longname_service(ServiceName, S),
+   findall(E, distinct(PID, service_entry_pid(S, E, PID)), Items),
+   sort_by(entry_sortkey, Items, SortedItems).
+
+entry_sortkey(E, k(SortedParents, Date)) :-
+   prop(E, broadcast(Date)),
+   findall(T-N, prop(E, parent(_, T, N)), Parents),
    sort_by(parent_priority, Parents, SortedParents).
 
 parent_priority(T-_, P) :- type_priority(T, P).
@@ -76,13 +74,13 @@ service_dir(S-Name) -->
    ;  []
    ).
 
-programme(ServiceName, _-E) -->
+programme(ServiceName, E) -->
 	{insist(entry_tags(ServiceName, E, PID, Tags, [])), pid_id(PID, Id)},
 	foldl(report, Tags), report('Id'-Id).
 
 live_service_tags(_-SLN, [file-File, 'Title'-SLN]) :- path_file(['Live Radio', SLN], File).
-entry_tags(ServiceName, E, PID) -->
-	[file-File], {prop(E, pid(PID)), path_file([ServiceName, PID], File)},
+entry_tags(Dir, E, PID) -->
+	[file-File], {prop(E, pid(PID)), path_file([Dir, PID], File)},
    tag(title_and_maybe_album, E), foldl(maybe, [tag(broadcast, E), tag(availability, E)]),
 	['Comment'-Syn, duration-Dur], {maplist(prop(E), [synopsis(Syn), duration(Dur)])}.
 
@@ -103,10 +101,9 @@ db_stats([artists-1, albums-M, songs-N, db_playtime-Dur]) :-
    findall(B-D, distinct(PID, pid_brand_dur(PID, B, D)), Items), length(Items, N),
    aggregate_all(count, distinct(PP,  member(just(PP-_)-_, Items)), M),
    aggregate_all(sum(D), member(_-D, Items), Dur).
-pid_brand_dur(PID, B, D) :-
-   service_entry(_, E), entry_maybe_parent('Brand', E, B),
-   prop(E, pid(PID)), prop(E, duration(D)).
+pid_brand_dur(PID, B, D) :- service_entry_pid(_, E, PID), entry_maybe_parent('Brand', E, B), prop(E, duration(D)).
 
+service_entry_pid(S, E, PID) :- service_entry(S, E), prop(E, pid(PID)).
 update_db([]) :- forall(service(S, _, _), update_service(S)).
 update_db([ServiceName]) :- service(S, _, ServiceName), update_service(S).
 all_services(Services) :- findall(S-SLN, service(S, _, SLN), Services).
