67517b25eed17338af8cae364b53c9d9fe74a1cd
samer-- <samer.abdallah.00@gmail.com>
2019-01-25 01:00:38 +0000

    Improve sorting and labeling by brand and series

--- prolog/swimpd/database.pl
+++ prolog/swimpd/database.pl
@@ -11,16 +11,17 @@
       Allow adding arbitrary PID
       Try putting PID/ID in multiple places in file tree
       memoise db stats: number of programmes, number of brands
+      confirm URL is ok and try another if not.
  */
 
 :- use_module(library(xpath)).
 :- use_module(library(memo)).
-:- use_module(library(dcg_core), [maybe/3]).
+:- use_module(library(dcg_core),  [maybe/3]).
 :- use_module(asyncu, [spawn/1]).
-:- use_module(tools,  [report//1, report//2, maybe/2, maybe//2]).
+:- use_module(tools,  [sort_by/3, report//1, report//2, maybe/2, maybe//2]).
 :- use_module(bbc(bbc_tools), [log_and_succeed/1]).
 :- use_module(bbc(bbc_db), [service/3, time_service_schedule/3, service_schedule/2, service_live_url/2, service_entry/2,
-                            prop/2, entry_maybe_parent/2, entry_xurl/3, interval_times/3, service_parent_children/3]).
+                            prop/2, entry_maybe_parent/3, entry_xurl/3, interval_times/3, service_parent_children/3]).
 
 :- volatile_memo pid_id(+atom, -integer).
 pid_id(_, Id) :- flag(songid, Id, Id+1).
@@ -55,10 +56,20 @@ lsinfo([]) -->
 lsinfo(['Live Radio']) --> {live_services(Services)}, foldl(live_radio, Services).
 lsinfo([ServiceName]) -->
 	{ longname_service(ServiceName, S),
-     findall(Children, service_parent_children(S, _, Children), Families),
-     findall(E, (member(Es, Families), member(E, Es)), Items)
+     findall(Item, service_sortkey_entry(S, Item), Items),
+     keysort(Items, SortedItems)
 	},
-	foldl(programme(ServiceName), Items).
+	foldl(programme(ServiceName), SortedItems).
+
+service_sortkey_entry(S, k(SortedParents, Date)-E1) :-
+   bagof(E, (service_entry(S, E), prop(E, pid(_))), [E1|_]),
+   prop(E1, broadcast(Date)),
+   findall(T-N, prop(E1, parent(_, T, N)), Parents),
+   sort_by(parent_priority, Parents, SortedParents).
+
+parent_priority(T-_, P) :- type_priority(T, P).
+type_priority('Brand', 1).
+type_priority('Series', 2).
 
 live_radio(S-ServiceName) -->
    {live_service_tags(S-ServiceName, Tags), pid_id(S, Id)},
@@ -72,7 +83,7 @@ service_dir(S-Name) -->
    ;  []
    ).
 
-programme(ServiceName, E) -->
+programme(ServiceName, _-E) -->
 	{ insist(entry_tags(ServiceName, E, PID, Tags, [])), pid_id(PID, Id)},
 	foldl(report, Tags), report('Id'-Id).
 
@@ -85,7 +96,7 @@ entry_tags(ServiceName, E, PID) -->
 tag(broadcast, E)    --> {prop(E, broadcast(B)), interval_times(B,T,_), ts_string(T,Broadcast)}, ['Date'-Broadcast].
 tag(availability, E) --> {prop(E, availability(A)), interval_times(A,_,T), ts_string(T,Until)}, ['AvailableUntil'-Until].
 tag(title_and_maybe_album, E) -->
-   {prop(E, title(FullTitle)), entry_maybe_parent(E, Parent)},
+   {prop(E, title(FullTitle)), entry_maybe_parent('Brand', E, Parent)},
    {maybe(cut_parent, Parent,  FullTitle, Title)}, ['Title'-Title],
    maybe(parent_as_album, Parent).
 
--- prolog/swimpd/tools.pl
+++ prolog/swimpd/tools.pl
@@ -1,6 +1,7 @@
 :- module(tools, [(+)//1, (*)//1, parse_head//2, nat//1, num//1, atom//1, quoted//1, quoted//2, in/2, fnth/5,
                   decimal//0, report//1, report//2, select_nth/4, maybe//2, maybe/2, fmaybe/3, fjust/3, flip/4]).
 
+:- use_module(library(listutils), [zip/3]).
 :- use_module(library(dcg_core), [(//)//2, list//1]).
 :- use_module(library(dcg_codes), [esc//2, ctype//1, fmt//2]).
 :- use_module(library(snobol), [notany//1, break//1]).
@@ -15,6 +16,9 @@ select_nth(N, X, L1, L2) :- nth0(N, L1, X, L2).
 fnth(0, X, Y, [X|L], [Y|L]).
 fnth(N, X, Y, [Z|L1], [Z|L2]) :- succ(M, N), fnth(M, X, Y, L1, L2).
 flip(P, X, Y, Z) :- call(P, Y, X, Z).
+
+sort_by(P, X1, X2) :- maplist(pairf(P), X1, KX1), keysort(KX1, KX2), zip(_, X2, KX2).
+pairf(P, X, Y-X) :- call(P, X, Y).
 
 :- meta_predicate +(//,?,?), +(//,?,?), quoted(3,?,?,?), quoted(//,?,?).
 +(P) --> call_dcg(P), *(P).
